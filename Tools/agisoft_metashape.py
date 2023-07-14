508dd76fe7b0f8755f19e1a2ba251b6949a7c07f
Lukas Olson <lukasdragon@live.ca>
2023-07-14 15:03:21 -0700

    Added ability to load already created project

--- Tools/agisoft_metashape.py
+++ Tools/agisoft_metashape.py
@@ -7,14 +7,29 @@ import os
 from helpers import get_all_files
 
 
-def create_metashape_project(data: Path):
+def create_or_load_metashape_project(data: Path):
+    project_path = data / "metashape_project.psx"
+    project_path = project_path.resolve()
+
+    # Create a Metashape document
     doc = Metashape.Document()
-    doc.save(path=str((data / "metashape_project.psx").resolve()))
-    doc.read_only = False
+    loaded = False
 
-    chunk = doc.addChunk()
+    # If the project file already exists, load it
+    if project_path.exists():
+        print("Loading existing project")
+        doc.open(str(project_path))
+        # Assuming that you want to work with the first chunk in the project
+        chunk = doc.chunk if len(doc.chunks) > 0 else doc.addChunk()
+        loaded = True
+    else:
+        # Else create and save a new project
+        print("Creating new project")
+        doc.save(path=str(project_path))
+        doc.read_only = False
+        chunk = doc.addChunk()
 
-    return doc, chunk
+    return doc, chunk, loaded
 
 
 def add_frames_to_chunk(chunk: Metashape.Chunk, frames: List[Path]):
@@ -46,15 +61,21 @@ def handle_error(e: Exception):
 
 
 def process_frames(data: Path, frames: Path, mask_path: Union[Path, None] = None,
-                   use_mask: bool = False, set_size: int = 100, overlap: int = 10):
+                   use_mask: bool = False, set_size: int = 250, overlap: int = 60):
     frames = sorted(get_all_files(frames, "*"))
     if not frames:
         print("No frames found in the specified path.")
         return
 
-    doc, chunk = create_metashape_project(data)
-    add_frames_to_chunk(chunk, frames)
-    load_masks(chunk, mask_path, use_mask)
+    doc, chunk, loaded = create_or_load_metashape_project(data)
+
+    if len(chunk.cameras) > 0:
+        print("Chunk already has cameras, skipping adding frames.")
+    else:
+        add_frames_to_chunk(chunk, frames)
+        load_masks(chunk, mask_path, use_mask)
+
+    doc.save()
 
     total_sets = len(frames) // set_size + (len(frames) % set_size > 0)
     match_window = set_size + overlap
@@ -87,6 +108,8 @@ def process_frames(data: Path, frames: Path, mask_path: Union[Path, None] = None
         except Exception as e:
             handle_error(e)
 
+        doc.save()
+
         pbar.update(1)
 
     pbar.close()
@@ -107,13 +130,15 @@ def process_frames(data: Path, frames: Path, mask_path: Union[Path, None] = None
             selected_count += 1
 
         if selected_count >= 100:
-            chunk.resetAlignment()
-            chunk.alignCameras()
+            chunk.alignCameras(reset_alignment=True)
             selected_count = 0
 
+            doc.save()
+
     if selected_count > 0:
-        chunk.resetAlignment()
-        chunk.alignCameras()
+        chunk.alignCameras(reset_alignment=True)
+
+    doc.save()
 
     print("Final optimization...")
     chunk.optimizeCameras()
